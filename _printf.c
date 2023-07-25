b73cd558603b35d34f6da9d34debf33786558a6f
calebmkw <calebmkw@gmail.com>
2023-07-25 13:38:15 -0700

    prnitf first commit

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - this is printf function
+ * @format: it is a format.
+ * Return: the printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -26,7 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			/* now write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -53,9 +53,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - this prints the contents of the buffer if it exist
+ * @buffer: an array of chars
+ * @buff_ind: the index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
